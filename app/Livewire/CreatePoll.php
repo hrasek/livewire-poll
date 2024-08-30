28dcd91290be39fbe235410c6d3bc3721b12b263
hrasek <hosekmotor@seznam.cz>
2024-08-30 11:15:49 +0200

    Added some basic livewire actions.

---
+++ app/Livewire/CreatePoll.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreatePoll extends Component
+{
+    public $title;
+    public $options = ['First'];
+    public function render()
+    {
+        return view('livewire.create-poll');
+    }
+public function addOption()
+{
+    $this->options[] = '';
+}
+}
