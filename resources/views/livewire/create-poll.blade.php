961a763d381ea5b523ea95403c3820770932014c
hrasek <hosekmotor@seznam.cz>
2024-09-17 16:06:20 +0200

    feat: Added data validation.

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -4,6 +4,10 @@
 
         <input type="text" wire:model.live="title" />
 
+        @error("title")
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
@@ -14,8 +18,12 @@
                 <label>Option {{$index + 1}}</label>
                 <div class="flex gap-2">
                     <input type="text" wire:model="options.{{$index}}">
+
                     <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                 </div>
+                @error("options.{$index}")
+                <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
 
 
